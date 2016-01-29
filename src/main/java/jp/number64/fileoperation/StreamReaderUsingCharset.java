aa5c593813069330e8df6af66c9742b7e17b5e30
TODOROKI Daisuke <number64tech@gmail.com>
2016-01-29 10:58:32 +0900

    Store temporarily (rebase later)

--- src/main/java/jp/number64/fileoperation/StreamReaderUsingCharset.java
+++ src/main/java/jp/number64/fileoperation/StreamReaderUsingCharset.java
@@ -20,50 +20,45 @@ import org.slf4j.LoggerFactory;
 public class StreamReaderUsingCharset {
     private static final Logger LOGGER = LoggerFactory.getLogger(StreamReaderUsingCharset.class);
 
-    /** Input file as Specified Charset, Return chars as (Unicode) CharBuffer.
+    /** CharBuffer default capacity */
+    private static final int capacity = 100;
+
+    /** Input file as Specified Charset, Return chars as (Unicode) CharBuffer. <br>
+     * three-piece set. somewhat classical... <br>
+     * when reader#close() is called by try-with-resources syntax, reader#close() calls InputStreamReader#close(), <br>
+     * and InputStreamReader#close() calls FileInputStream#close(). <br>
      * @param charset charset of input target textfile
      * @param targetFile input target textfile
-     * @param bufferSize size of {@link CharBuffer#allocate(int)}
      */
-    public String inputAsSpecifiedCharSet(Charset charset, CheckedFile targetFile) throws IOException {
-        //CharBuffer result = CharBuffer. CharBuffer();
+    public CharBuffer inputAsSpecifiedCharSet(Charset charset, CheckedFile targetFile) throws IOException {
+        CharBuffer result = CharBuffer.allocate(capacity);
 
-        // three-piece set. somewhat classical...
-        // when reader#close() is called by try-with-resources syntax, reader#close() calls InputStreamReader#close(),
-        // and InputStreamReader#close() calls FileInputStream#close().
         try (BufferedReader reader =
           new BufferedReader(new InputStreamReader(new FileInputStream(targetFile), charset))) {
-            while(true) {
-                Integer readResult = Integer.valueOf(reader.read());
-                if (readResult == -1) {
-                    break;
-                }
-                LOGGER.debug("readResult: {} {}", Integer.toHexString(readResult), Character.getName(readResult));
-            }
+            int readCount = reader.read(result);
+            LOGGER.debug("** readCount: {}", readCount);
         } catch (IOException e) {
             throw e;
         }
 
-        return null;
-    }
-
-    public String convertToEncodedBytesString(Charset charset, String input) {
-        return null;
+        // after reading, position indicates last-char. rewind before return this.
+        result.rewind();
+        return result;
     }
 
     /** ShiftJIS */
-    public String inputAsShiftJISWinExtended(CheckedFile targetFile) throws IOException {
+    public CharBuffer inputAsShiftJISWinExtended(CheckedFile targetFile) throws IOException {
         LOGGER.debug("** start #inputAsShiftJISWinExtended()");
         Charset charset = Charset.forName("Windows-31j");
-        String result = inputAsSpecifiedCharSet(charset, targetFile);
+        CharBuffer result = inputAsSpecifiedCharSet(charset, targetFile);
         return result;
     }
 
     /** EUC-JP */
-    public String inputAsEucJp(CheckedFile targetFile) throws IOException {
+    public CharBuffer inputAsEucJp(CheckedFile targetFile) throws IOException {
         LOGGER.debug("** start #inputAsEucJp()");
         Charset charset = Charset.forName("EUC-JP");
-        String result = inputAsSpecifiedCharSet(charset, targetFile);
+        CharBuffer result = inputAsSpecifiedCharSet(charset, targetFile);
         return result;
     }
 
--- src/test/java/jp/number64/fileoperation/StreamReaderUsingCharsetTest.java
+++ src/test/java/jp/number64/fileoperation/StreamReaderUsingCharsetTest.java
@@ -2,10 +2,14 @@ package jp.number64.fileoperation;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 
 import org.junit.FixMethodOrder;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
@@ -23,26 +27,78 @@ public class StreamReaderUsingCharsetTest {
     @FixMethodOrder(MethodSorters.NAME_ASCENDING)
     public static final class ShiftJISInputTest extends FileTestBase {
         private static final Logger LOGGER = LoggerFactory.getLogger(ShiftJISInputTest.class);
+        private static final Charset USING_CHARSET = Charset.forName("Windows-31j");
 
         @Test
         public void case01() throws IOException {
             LOGGER.debug(START_LOG, getMethodName(new Throwable()));
-            CheckedFile checkedFilePath =
-                CheckedFile.generateCheckedFile("src/test/resources/fileoperation/tochigi_sjis_crlf.csv");
+            levelOffSandBox();
+            File testInput = new File(sandBox.getAbsolutePath() + File.separator + "sjis01.txt");
+            try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(testInput), USING_CHARSET)) {
+                writer.write("\u3066\u3059\u3068");
+            }
+            CheckedFile checkedFilePath = CheckedFile.generateCheckedFile(testInput.getAbsolutePath());
             StreamReaderUsingCharset testTarget = new StreamReaderUsingCharset();
-            String result = testTarget.inputAsShiftJISWinExtended(checkedFilePath);
+            CharBuffer result = testTarget.inputAsShiftJISWinExtended(checkedFilePath);
+
+            assertEquals("\u3066", String.valueOf(result.get()));
+            assertEquals("\u3059", String.valueOf(result.get()));
+            assertEquals("\u3068", String.valueOf(result.get()));
+            result.rewind();
+            assertEquals("\u3066", String.valueOf(result.get()));
+            result.mark();
+            assertEquals("\u3059", String.valueOf(result.get()));
+            assertEquals("\u3068", String.valueOf(result.get()));
+            result.reset();
+            assertEquals("\u3059", String.valueOf(result.get()));
         }
+
+        @Test
+        public void case02SurrogatePairHandling() throws IOException {
+            LOGGER.debug(START_LOG, getMethodName(new Throwable()));
+            levelOffSandBox();
+            File testInput = new File(sandBox.getAbsolutePath() + File.separator + "sjis02.txt");
+            try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(testInput), USING_CHARSET)) {
+                // "HOKKE"
+                writer.write("\ud867\ude3d");
+            }
+            CheckedFile checkedFilePath = CheckedFile.generateCheckedFile(testInput.getAbsolutePath());
+            StreamReaderUsingCharset testTarget = new StreamReaderUsingCharset();
+            CharBuffer result = testTarget.inputAsShiftJISWinExtended(checkedFilePath);
+
+            LOGGER.debug("1st: {}", (int)result.get());
+            LOGGER.debug("2nd: {}", result.get());
+            result.rewind();
+            assertEquals("\u3066", String.valueOf(result.get()));
+        }
+
     }
 
     /**  */
     @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-    public static final class UnicodeEscapeTest extends FileTestBase {
-        private static final Logger LOGGER = LoggerFactory.getLogger(UnicodeEscapeTest.class);
+    public static final class EucJpInputTest extends FileTestBase {
+        private static final Logger LOGGER = LoggerFactory.getLogger(EucJpInputTest.class);
+        private static final Charset USING_CHARSET = Charset.forName("EUC-JP");
 
         @Test
         public void case01() throws IOException {
             LOGGER.debug(START_LOG, getMethodName(new Throwable()));
-            assertEquals(null, "");
+            levelOffSandBox();
+            File testInput = new File(sandBox.getAbsolutePath() + File.separator + "euc.txt");
+            try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(testInput), USING_CHARSET)) {
+                writer.write("\u3044\u30fc\u3086\u30fc\u3057\u30fc");
+            }
+            CheckedFile checkedFilePath = CheckedFile.generateCheckedFile(testInput.getAbsolutePath());
+            StreamReaderUsingCharset testTarget = new StreamReaderUsingCharset();
+            CharBuffer result = testTarget.inputAsShiftJISWinExtended(checkedFilePath);
+
+            assertEquals("\u3044", String.valueOf(result.get()));
+            assertEquals("\u30fc", String.valueOf(result.get()));
+            assertEquals("\u3086", String.valueOf(result.get()));
+            assertEquals("\u30fc", String.valueOf(result.get()));
+            assertEquals("\u3057", String.valueOf(result.get()));
+            assertEquals("\u30fc", String.valueOf(result.get()));
+            assertEquals(0, result.get());
         }
     }
 
@@ -50,10 +106,22 @@ public class StreamReaderUsingCharsetTest {
 
     /**  */
     public static final class ListUp {
-        @Ignore
-        @Test
-        public void doIt() {
+        public void experiment() {
             StreamReaderUsingCharset.listupAvailabeCharsets();
+        }
+    }
+
+    /**  */
+    public static final class Dump {
+        private static final Logger LOGGER = LoggerFactory.getLogger(Dump.class);
+        public void experiment() throws IOException {
+            CheckedFile checkedFilePath =
+                CheckedFile.generateCheckedFile("src/test/resources/fileoperation/tochigi_sjis_crlf.csv");
+            StreamReaderUsingCharset testTarget = new StreamReaderUsingCharset();
+            CharBuffer result = testTarget.inputAsShiftJISWinExtended(checkedFilePath);
+            while(result.hasRemaining()) {
+                LOGGER.debug("readResult: {}", String.valueOf(result.get()));
+            }
         }
     }
 }
